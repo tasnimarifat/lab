465dab79b4ac1bf49b23ae2a646c9c6e43ed858d
tasnimarifat <abirmuhaimin2@gmail.com>
2026-04-24 18:02:52 +0600

    Create Swap.java

---
+++ Swap.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner; 
+public class Swap {
+    public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a: ");
+        int a=sc.nextInt();
+        System.out.print("Enter b: ");
+        int b=sc.nextInt();
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println("a="+a);
+ System.out.println("b="+b);   }     }
